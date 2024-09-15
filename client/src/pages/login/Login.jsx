7cde4b75d4036162127cdc3b20d3be7a44ea79d1
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-09-15 20:38:18 +0530

    Update Login.jsx

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://trailer-time-server-api.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
