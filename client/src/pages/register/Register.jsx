c3437c50a270618413a17d1d61655f0cc83b5644
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-09-15 20:38:41 +0530

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://trailer-time-server-api.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
