5e5e623942589d584df2c6f4258cb15ba93ae23c
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-09-16 20:31:40 +0530

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -21,7 +21,7 @@ const Register = () => {
     setLoading(true); 
 
     try {
-      const response = await fetch("https://trailer-time-server.vercel.app/register", {
+      const response = await fetch("https://trailer-time-server-api.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
